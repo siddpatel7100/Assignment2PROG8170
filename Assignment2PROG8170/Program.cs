f18681ebb627c1e610d0268af0c5c52495459387
Siddharth Patel <spatel8266@conestogac.on.ca>
2021-02-21 15:37:08 -0500

    Add project files.

---
+++ Assignment2PROG8170/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assignment2PROG8170
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+             int a, b, c;
+            Console.WriteLine("1.Enter Triangle Dimensions");
+            Console.WriteLine("2.Exit");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 1st angle side: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2st angle side: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 3st angle side: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            TriangleSolver.analyze(a,b,c);
+        }
+    }
+}
---
+++ Assignment2PROG8170/TriangleSolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2PROG8170
+{
+   static public class TriangleSolver
+    {
+        static int sum;
+
+        public static string analyze(int a,int b,int c)
+        {
+           
+            
+            sum = a + b + c;
+
+            if(a + b <= c || a + c <= b || b + c <= a)
+
+            {
+                return("The triangle is not valid.\n");
+            }
+            else
+            {
+
+                return ("The triangle is valid.\n");
+            }
+
+            if(a==b && b == c)
+            {
+                return ("This is an equilateral triangle.\n");
+            }else if(a==b || a==c || b == c)
+            {
+                return ("This is an isosceles triangle.\n");
+            }
+            else
+            {
+                return ("This is a scalene triangle.\n");
+            }
+
+        }
+       
+         
+        
+    }
+}
