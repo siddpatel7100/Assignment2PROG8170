55841bb5cdd37e6a3eb970e8c32091a80c1c9cf0
Siddharth Patel <spatel8266@conestogac.on.ca>
2021-02-23 14:41:24 -0500

    Updated all eigth unit test cases

--- Assignment2PROG8170/TriangleSolver.cs
+++ Assignment2PROG8170/TriangleSolver.cs
@@ -13,8 +13,23 @@ namespace Assignment2PROG8170
            
             
             sum = a + b + c;
-
-            if(a + b <= c || a + c <= b || b + c <= a)
+            if (a < 0 && b>0 && c>0)
+            {
+                return "Side One Invalid";
+            }
+            if (b < 0 && a>0 && c>0)
+            {
+                return "Side Two Invalid";
+            }
+            if (c < 0 && a>0 && b>0)
+            {
+                return "Side Three Invalid";
+            }
+            if(a < 0 && b < 0 && c < 0)
+            {
+                return "All sides are negative";
+            }
+            if (a + b <= c || a + c <= b || b + c <= a)
 
             {
                 return("The triangle is not valid.\n");
--- TriangleUnitTest1/TriangleSolverTests.cs
+++ TriangleUnitTest1/TriangleSolverTests.cs
@@ -11,28 +11,52 @@ namespace Assignment2PROG8170.Tests
     [TestClass()]
     public class TriangleSolverTests
     {
-        [TestMethod()]
+        [TestMethod()]//It will check user entered number and display message if any input is not forming a valid traingle
         public void analyzeTriangleValidityTest_NotValid()
         {
             Assert.That(TriangleSolver.analyze(2, 3, 8), Is.EqualTo("The triangle is not valid.\n"));
         }
 
-        [TestMethod()]
-        public void analyzeEquilateraltriangleTest()
+        [TestMethod()]//Will check input number forming an Equilateral Triangle
+        public void analyzeEquilateraltriangleTest_IsTrue()
         {
             Assert.That(TriangleSolver.analyze(25, 25, 25), Is.EqualTo("This is an equilateral triangle.\n"));
         }
 
-        [TestMethod()]
-        public void analyzeIsoscelestriangleTest()
+        [TestMethod()]//Will check input number forming an Isosceles Triangle
+        public void analyzeIsoscelestriangleTest_IsTrue()
         {
             Assert.That(TriangleSolver.analyze(70, 70, 40), Is.EqualTo("This is an isosceles triangle.\n"));
         }
 
-        [TestMethod()]
-        public void analyzescalenetriangleTest()
+        [TestMethod()]//Will check input number forming a Scalene Triangle
+        public void analyzeScalenetriangleTest_IsTrue()
         {
             Assert.That(TriangleSolver.analyze(7, 12, 15), Is.EqualTo("This is a scalene triangle.\n"));
+        }
+
+        [TestMethod()]// Will verify first side is a valid number i.e. not negative,if it is then it will display an error
+        public void sideOneNumberValidityCheck_IsNegative()
+        {
+            Assert.That(TriangleSolver.analyze(-5,5,6),Is.EqualTo("Side One Invalid"));
+        }
+
+        [TestMethod()]// Will verify Second side is a valid number i.e. not negative,if it is then it will display an error
+        public void sideTwoNumberValidityCheck_IsNegative()
+        {
+            Assert.That(TriangleSolver.analyze(8,-10,2), Is.EqualTo("Side Two Invalid"));
+        }
+
+        [TestMethod()]// Will verify Third side is a valid number i.e. not negative,if it is then it will display an error
+        public void sideThreeNumberValidityCheck_IsNegative()
+        {
+            Assert.That(TriangleSolver.analyze(7, 4, -9), Is.EqualTo("Side Three Invalid"));
+        }
+
+        [TestMethod()]// Will verify all sides is a valid number i.e. not negative,if it is then it will display an error
+        public void allSidesNumberValidityCheck_IsNegative()
+        {
+            Assert.That(TriangleSolver.analyze(-9,-8,-15), Is.EqualTo("All sides are negative"));
         }
     }
 }
